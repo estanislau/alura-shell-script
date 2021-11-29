1d8b39f7c464bde9ea0e446ad46f81ad75fac60f
lao <estanislau@gmail.com>
2021-11-29 16:28:44 -0300

    Criado script conversao-jpg-png.sh

---
+++ conversao-jpg-png.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/documents/git/alura/shell-script/scripts
+
+if [ ! -d png ]
+then
+    mkdir png
+fi
+   
+for imagem in *.jpg
+do
+    imagem_sem_extensao=$(ls $imagem | awk -F. '{print $1}')
+    convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+done
